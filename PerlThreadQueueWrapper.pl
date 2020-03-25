612a697afd6880dec312767a67d78b5f7024c439
fulxie <fulxie@users.noreply.github.com>
2020-03-25 13:06:02 -0400

    Update PerlThreadQueueWrapper.pl

--- PerlThreadQueueWrapper.pl
+++ PerlThreadQueueWrapper.pl
@@ -7,37 +7,64 @@
 #Wrap Thread Queue
 sub RunJobs
 {
+	############### example of usage #############
+	### what you need to do is to define your job function and prepare parameter for each job
+	# # sub DoYourJob{
+	# # ## get arguments 
+	# # my @args=@_; 
+	# # ####do job here
+	# # print    $args[0], " sdf ".$args[1]."\n";
+
+	# # }
+
+	# # #prepare parameters for each job and pack to an array here
+	# # @jobs_arguments=(); 
+	# # foreach $i(1..100)
+	# # {
+	# #   ##for example
+	# #   my @aa=($i, "q".$i); # Two arguments
+	# #   push @jobs_arguments, \@aa; 
+	# # }
+
+	# # #run jobs
+	# # my $threads=20;
+	#
+	# # RunJobs($threads, \&DoYourJob, \@jobs_arguments);
+	############### example of usage #############	
+	
 	use threads;
 	use Thread::Queue;
-	our $NTHREADS = shift;	
+	our $threadCounts = shift;	
 	my  $jobName=shift;
-	my  $jobs=shift;
-	
+	my  $jobs_arguments=shift;
+
 	#define worker sub
-    my  $workerSub=sub{
+	my  $workerSub=sub{
 				  my $jobName=shift;
-				  our $PROCESS_QUEUE;
-				  while ( my ($ps) = $PROCESS_QUEUE -> dequeue(1)  )
+				  our $processQueue;
+				  while ( my ($args) = $processQueue -> dequeue(1)  )
 				  {
-					if(not defined $ps)
+					if(not defined $args)
 					{
 					  last;
 					}	
-					eval ($jobName.'($ps)'); #execute a job here
+					$jobName->(@$args); #execute a job here
+					#eval ($jobName.'($ps)'); #execute a job here for RunJobs($threads, "DoYourJob", \@jobs); 
 					sleep(1);	
 					#print threads -> self() -> tid(). ": pinging $server\n"; 
-				  }};	
-	our $PROCESS_QUEUE = Thread::Queue -> new();
-	#our $failed_q = Thread::Queue -> new();
-	my  @jobss=@$jobs;
+				  }};
+				  
+	our $processQueue = Thread::Queue -> new();
+	my  @jobss=@$jobs_arguments;
 	my $jobsCount=scalar @jobss;
 	my $i;
 	foreach $i(0..($jobsCount-1))
 	{
-	  $PROCESS_QUEUE -> enqueue (\@{$jobss[$i]});
+	  $processQueue -> enqueue (\@{$jobss[$i]});
 	}
-	$PROCESS_QUEUE -> end();	
-	for (1..$NTHREADS)
+	
+	$processQueue -> end();	
+	for (1..$threadCounts)
 	{
 	  threads ->create($workerSub, $jobName);
 	}
@@ -46,7 +73,7 @@ sub RunJobs
 	{
 	  $thr -> join();
 	}
-
+	#failed queues
 	# while ( my $string = $failed_q -> dequeue_nb() )
 	# {
 	# print "$string failed to ping\n";
@@ -56,14 +83,8 @@ sub RunJobs
 
 ### what you need to do is to define your job function and prepare parameter for each job
 sub DoYourJob{
-	my $arg_=shift;
-	
-	## get arguments 
-	my @args=@$arg_; 
-	
-	####do job here
-	print    $args[0], " sdf ".$args[1]."\n";
-	 
+	my @args=@_; 		 
+	 print    $args[0], " sdf ".$args[1]."\n";	 
 }
 
 #prepare parameters for each job and pack to an array here
@@ -71,13 +92,14 @@ sub DoYourJob{
 foreach $i(1..100)
 {
   ##for example
-  my @aa=($i, "q".$i);
+   my @aa=($i, "q".$i, "xiexie".$i); # three arguments for DoYourJob
+ 
   push @jobs, \@aa; 
 }
 
 #run jobs
 my $threads=20;
-RunJobs($threads, "DoYourJob", \@jobs);
+RunJobs($threads, \&DoYourJob, \@jobs);
 
 
 
